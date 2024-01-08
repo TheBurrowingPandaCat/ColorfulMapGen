f49eec75eaec9054ccf82ee39583ccba2335466f
TheBurrowingPandaCat <theburrowingpandacat@badmaonu.mailer.me>
2024-01-08 18:07:20 -0500

    initialized colorblockprint package

---
+++ pkg/colorblockprint/colorblockprint.go
@@ -0,0 +1,6 @@
+package colorblockprint
+
+func PrintBlocks() {
+	fullBlockCharacter := "\u2558"
+	println(fullBlockCharacter + " " + fullBlockCharacter + fullBlockCharacter + " " + fullBlockCharacter + fullBlockCharacter + fullBlockCharacter)
+}
