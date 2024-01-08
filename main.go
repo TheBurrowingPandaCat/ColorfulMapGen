49c9e13535bdfc1705cc94833f2144f2c1a84f83
TheBurrowingPandaCat <theburrowingpandacat@badmaonu.mailer.me>
2024-01-08 18:17:59 -0500

    Imported colorblockprint into main for basic functionality

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	
+	blocks "github.com/TheBurrowingPandaCat/ColorfulMapGen/pkg/colorblockprint"
 )
 
 func main() {
 	fmt.Println("Woo! Init!")
+	blocks.PrintBlocks()
 }
