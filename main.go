838f7dafc81bef769915d61ca31b1723ef01d70e
TheBurrowingPandaCat <theburrowingpandacat@badmaonu.mailer.me>
2024-01-08 19:32:20 -0500

    Added colors and simplified main file

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-	
 	blocks "github.com/TheBurrowingPandaCat/ColorfulMapGen/pkg/colorblockprint"
 )
 
 func main() {
-	fmt.Println("Woo! Init!")
 	blocks.PrintBlocks()
 }
--- pkg/colorblockprint/colorblockprint.go
+++ pkg/colorblockprint/colorblockprint.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+const (
+	Black byte = iota + 12
+	Red
+	Green
+	Yellow
+	Blue
+	Magenta
+	Cyan
+	White
+)
+
 var blocks map[string]string = map[string]string{
 	"full":   "\u2588",
 	"dark":   "\u2593",
@@ -12,8 +23,37 @@ var blocks map[string]string = map[string]string{
 	"empty":  " ",
 }
 
+var resetCharacter string = "\033[0m"
+
 func PrintBlocks() {
-	fmt.Println(blocks["full"] + blocks["full"] + blocks["empty"] + blocks["light"] +
+	fmt.Println(ColorChangingCharacter(Blue, Magenta) + blocks["full"] + blocks["full"] + blocks["empty"] + blocks["light"] +
 		blocks["medium"] + blocks["dark"] + blocks["full"] + blocks["dark"] + blocks["medium"] +
-		blocks["light"] + blocks["empty"] + blocks["full"] + blocks["full"])
+		blocks["light"] + blocks["empty"] + blocks["full"] + blocks["full"] + resetCharacter)
+}
+
+func ColorChangingCharacter(foregroundColor byte, backgroundColor byte) string {
+	foreground := map[byte]string{
+		Black:   "30",
+		Red:     "31",
+		Green:   "32",
+		Yellow:  "33",
+		Blue:    "34",
+		Magenta: "35",
+		Cyan:    "36",
+		White:   "37",
+	}
+
+	background := map[byte]string{
+		Black:   "40",
+		Red:     "41",
+		Green:   "42",
+		Yellow:  "43",
+		Blue:    "44",
+		Magenta: "45",
+		Cyan:    "46",
+		White:   "47",
+	}
+
+	return "\033[" + foreground[foregroundColor] + ";" +
+		background[backgroundColor] + "m"
 }
